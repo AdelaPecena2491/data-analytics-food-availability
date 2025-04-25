2c11cc8eae9ada39cc59c3bc4f5f731017296261
AdelaPecena2491 <142669109+AdelaPecena2491@users.noreply.github.com>
2025-04-25 20:08:36 +0200

    Create t_adela_pecena_project_SQL_primary_final.sql

---
+++ data/final_tables/t_adela_pecena_project_SQL_primary_final.sql
@@ -0,0 +1,11 @@
+data/final_tables/t_adela_pecena_project_SQL_primary_final.sql
+
+/data
+  └── final_tables/
+        ├── t_adela_pecena_project_SQL_primary_final.sql
+        └── t_adela_pecena_project_SQL_secondary_final.sql
+
+/sql_queries/
+  └── questions.sql
+
+/README.md
